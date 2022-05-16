d3d25daf6ddbc2180c3b18a7f4536e5737be2554
enigma6174 <enigma6174@outlook.com>
2022-05-16 23:23:05 +0530

    Project: Added Basic Routes

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "DUMMY_POST_DATA"}
+
+
+@app.post("/create/posts")
+def create_posts(payload: dict = Body(...)):
+    print(f'[INFO] payload: {payload}')
+    return {"new_post": {"title": payload["title"], "content": payload["content"]}}
+
+
